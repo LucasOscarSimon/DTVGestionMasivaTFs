e50487a7c3f7c6b2b44b5af3787cc42860f84ebc
Federico Morales <federico.morales@neoris.cxnetworks.net>
2018-10-23 14:39:18 -0300

    Maquetado de Consulta de Auditoria.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { LoginComponent } from '../layout/components/login/login.component';
 import { AjusteMasivoComponent } from '../layout/components/ajuste-masivo/ajuste-masivo.component';
-
+import { ConsultaAuditoriaComponent } from '../layout/components/consulta-auditoria/consulta-auditoria.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'ajuste-masivo', component: AjusteMasivoComponent },
+  { path: 'consulta-auditoria', component: ConsultaAuditoriaComponent },
   { path: '**', redirectTo: 'login'}
 ];
 
